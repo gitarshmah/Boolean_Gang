85a61eda41d8359b141bdaf7f8385006db4d1a13
Vidip Ghosh <91741581+Vidip-Ghosh@users.noreply.github.com>
2023-02-12 14:58:29 +0530

    Delete LoginPage directory

--- LoginPage/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class InputForm(forms.Form):
-    # name = forms.CharField(max_length=200)
-    username = forms.EmailField(label='username', max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.EmailField(max_length=100)
-    confirmPassword = forms.CharField(widget=forms.PasswordInput())
